97979257c4f82c99c46350e33e01ab303e1974ef
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-03-20 13:12:25 +0000

    done variables and data types

--- 2-JS-basics/starter/greet.js
+++ 2-JS-basics/starter/greet.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+var firstName = "Emanuel";
+console.log(firstName);
+
+var lastName = "Moniz";
+var age = 30;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+
+console.log(job);
+
+job = "teacher";
+
+console.log(job);
