506511994884376cdcd5f6f8baf7640077f52aee
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-03-20 13:26:47 +0000

    comments and concatenation

--- 2-JS-basics/starter/greet.js
+++ 2-JS-basics/starter/greet.js
@@ -1,3 +1,8 @@
+/*
+* variables and data types
+*/
+
+/*
 var firstName = "Emanuel";
 console.log(firstName);
 
@@ -7,10 +12,30 @@ var age = 30;
 var fullAge = true;
 console.log(fullAge);
 
-var job;
+var job; // varable without assignment
 
-console.log(job);
+console.log(job); // print undefined
 
 job = "teacher";
 
 console.log(job);
+*/
+
+var firstName = "Emanuel";
+var age = 31;
+
+console.log(firstName + " " + age);
+
+var job, isMarried;
+job = "Full Stack Developer";
+isMarried = false;
+
+console.log(firstName + " is a " + age + " years old " + job + ". Is he Married? " + isMarried);
+
+age = "thirty";
+job = "web developer";
+
+alert(firstName + " is a " + age + " years old " + job + ". Is he Married? " + isMarried)
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + " " + lastName);
